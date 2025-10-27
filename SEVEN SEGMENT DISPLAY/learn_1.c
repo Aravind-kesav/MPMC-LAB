40f3cd792732556df32b3276d25ca930b6237816
Aravind Kesav <147848811+Aravind-kesav@users.noreply.github.com>
2025-10-27 23:49:15 +0530

    These files work with MPLAB Software
    No errors works perfectly fine  and print .EEE .GCT

---
+++ SEVEN SEGMENT DISPLAY/learn_1.c
@@ -0,0 +1,43 @@
+#include<p18f452.h>
+void delay(int);
+void main()
+{
+   int i,j;
+	int a[8]={128,121,121,121,128,61,57,120};
+	int b[8] = {8,4,2,1,8,4,2,1};
+    
+	TRISB=0;
+    TRISD=0;
+
+	while(1)
+	{
+	for(j=0;j<2000;j++)
+	{
+	for(i = 0; i<4;i++)
+	{
+   	PORTB=a[i];
+	PORTD=b[i];
+	delay(200);
+    	}
+	}
+	PORTD=0;
+	delay(62000);
+
+	for(j=0;j<2000;j++)
+	{
+	for(i=4;i<8;i++)
+	{
+   	PORTB=a[i];
+	PORTD=b[i];
+	delay(200);
+    }
+	}
+	PORTD=0;
+	delay(62000);
+	}
+}
+
+void delay(int j)
+{
+while(j--);
+}
