c3ad33f7126b1b98db5354c21712cd0d70b2a069
Aravind Kesav <147848811+Aravind-kesav@users.noreply.github.com>
2025-11-16 22:33:06 +0530

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,134 +1,178 @@
-#include<16F877A.h>
+/***************************************************/
+/* LCD Routines for 2 line X 16 Characters Display
+/***************************************************/
 
-#use delay(clock=20000000)
-#use rs232(baud=19200, xmit=PIN_C6, rcv=PIN_C7)
+#include "LPC214x.h"                       /* LPC214x definitions  */
 
-void busycheck();
+void WriteCommandLCD(unsigned char CommandByte);
+void WriteDataLCD(unsigned char DataByte);
+void LCDDelay(void);
+void LCDDelay1600(void);
+void SendByte(unsigned char Value);
+void InitializeLCD();
 
-unsigned int array[19]={"Vi Microsystems"};
-unsigned char array1[] = {"VPUT-01"};
-unsigned char array2[] = {"Embedded Trainer"};
-unsigned char a,i,b;
-
-void main()
-{
-
-        output_low(PIN_B3);
-        output_high(PIN_B1);
-
-        busycheck();
-        output_d(0x38);         /* Entry mode set */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-        busycheck();
-        output_d(0x06);         /* function set */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-        busycheck();
-        output_d(0x01);         /* clear display */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-        busycheck();
-        output_d(0x0c);         /* cursor on */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-        busycheck();
-        output_d(0x82);         /* starting address */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-        for (i=0;i<15;i++)
-        {
-                busycheck();
-                output_d(0x01);
-                output_low(PIN_B1);
-                output_high(PIN_B1);
-
-                b=array[i];
-                output_d(b);
-                output_high(PIN_B3);
-                output_low(PIN_B3);
-        }
-
-	 	busycheck();
-        output_d(0xc6);         /* starting address */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-		for (i=0;i<7;i++)
-        {
-                busycheck();
-                output_d(0x01);
-                output_low(PIN_B1);
-                output_high(PIN_B1);
-
-                b=array1[i];
-                output_d(b);
-                output_high(PIN_B3);
-                output_low(PIN_B3);
-        }
-
-
-		busycheck();
-        output_d(0x96);         /* starting address */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-		for (i=0;i<16;i++)
-        {
-                busycheck();
-                output_d(0x01);
-                output_low(PIN_B1);
-                output_high(PIN_B1);
-
-                b=array2[i];
-                output_d(b);
-                output_high(PIN_B3);
-                output_low(PIN_B3);
-        }
-
-		busycheck();
-        output_d(0xd4);         /* starting address */
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-
-		for (i=0;i<20;i++)
-        {
-                busycheck();
-                output_d(0x01);
-                output_low(PIN_B1);
-                output_high(PIN_B1);
-
-                
-                output_d('-');
-                output_high(PIN_B3);
-                output_low(PIN_B3);
-        }
+void DataAddressDirection(void);
+void DisplayLCD(char LineNumber,char *Message);
+void DisplayLCD2Digit(char LineNumber,char CharPosition,char Data);
 
 	
-stop:
-        goto stop;
+/* Initializes LCD */
+void InitializeLCD()
+{                         
+	DataAddressDirection();
+	IOSET0 = 0x00580000;			// Set A0, A1, A2 
+	
+	WriteCommandLCD(0x38);			//Command to select 8 bit interface
+	LCDDelay1600();
+
+	WriteCommandLCD(0x38);			//Command to select 8 bit interface
+	LCDDelay();						//Small delay
+	
+	WriteCommandLCD(0x38);			//Command to select 8 bit interface
+	LCDDelay();	
+		
+	WriteCommandLCD(0x0c);			//Command to on cursor,blink cursor
+	LCDDelay();	
+
+	WriteCommandLCD(0x06);			//Command for setting entry mode
+	LCDDelay();	
+
+	WriteCommandLCD(0x01);			//Clear LCD
+	LCDDelay1600();	
 }
 
 
-void busycheck()
+/* Writes a command byte to LCD */
+void WriteCommandLCD(unsigned char CommandByte)
 {
-        output_d(0x02);
-        output_low(PIN_B1);
-        output_high(PIN_B1);
-        delay_ms(2);
-busy:
-        output_high(PIN_B3);
-        output_low(PIN_B3);
-        a=input_d();
-        if ((a&0x80)==0x80)
-                goto busy;
-        output_d(0x0);
-        output_low(PIN_B1);
-        output_high(PIN_B1);
-        delay_us(10);
+	IOCLR1 = 0x03000000;			// Clear RS and RW 
+	SendByte(CommandByte);				
+	LCDDelay();						//Small delay
 }
+
+
+/* Send a byte of data to LCD */
+void SendByte(unsigned char Value)
+{	
+	IOPIN1 &= 0xff00ffff;
+	IOPIN1 |= Value << 16;		/* Write data to data bus */
+	IOSET0 = 0x00100000;		/* Generate chip enable signal for LCD */
+	IOCLR0 = 0x00480000;
+	LCDDelay();
+	IOSET0 = 0x00580000;		/* Set A0, A1 & A2 to disable LCD */
+	LCDDelay();
+}
+
+
+/* Writes a Data byte to LCD */
+void WriteDataLCD(unsigned char DataByte)
+{
+	IOCLR1 = 0x01000000;			/* clear RW */
+	IOSET1 = 0x02000000;			/* Set RS */
+	SendByte(DataByte);
+	LCDDelay();						//Small delay
+}
+
+
+/* Small delay */
+void LCDDelay(void)
+{
+	int	a;
+	for(a=0;a<0x1000;a++);
+} 
+
+/* Big delay */
+void LCDDelay1600(void)
+{
+	long	a;
+	for(a=0;a<0x050000;a++);
+} 
+
+
+/* Makes cursor visible */
+void CursorON(void)
+{
+	WriteCommandLCD(0x0f);			//Command to switch on cursor
+}
+
+
+
+/* Makes cursor invisible */
+void CursorOFF(void)
+{
+	WriteCommandLCD(0x0c);			//Command to switch off cursor
+}
+
+
+
+void DisplayLCD2Digit(char LineNumber,char CharPosition,char Data)
+{
+	unsigned char a;
+	if(LineNumber ==0)
+	{	//First Line
+		a = 0x80;						//command for first line selection				
+	}
+	else
+	{	//Second line
+		a = 0xc0;						//command for second line selection
+	}
+	a+=(CharPosition);					//Calculate the character position
+	WriteCommandLCD(a);					//Send command to select the given digit
+	if( (Data & 0xf0) < 0xa0)			//Check for less than 0xa0
+	{
+		a = ((Data & 0xf0) >> 4) + '0';	//Get the ASCII character
+	}
+	else
+	{	
+		a = ((Data & 0xf0) >> 4) + 'A'- 0x0a;	
+										//Get the ASCII character
+	}
+	WriteDataLCD(a);					//Display the first character
+	if( (Data & 0x0f) < 0x0a)   		//Check for less tham 0x0a
+	{
+		a = (Data & 0x0f)+'0';			//Get the ASCII character
+	}
+	else
+	{
+		a = (Data & 0x0f)+'A' - 0x0a;	//Get the ASCII character
+	}
+	WriteDataLCD(a);
+}	
+
+/* Displays a message on LCD */
+void DisplayLCD(char LineNumber,char *Message)
+{
+	int	a;
+	if(LineNumber ==0)
+	{	//First Line
+		WriteCommandLCD(0x80);		//Select the first line
+	}
+	else
+	{	//Second line
+		WriteCommandLCD(0xc0);		//Select the second line
+	}
+	while(*Message)
+	{
+		WriteDataLCD(*Message);		//Display a character
+		Message++;					//Increment pointer
+	}
+} 
+ 
+   
+void DataAddressDirection(void)
+{
+	IODIR0 |= 0x00580000;			// Set A0, A1, A2 output lines
+ 	IODIR1 |= 0x03ff0000;
+}
+
+
+
+void __gccmain()
+{
+
+}
+
+
+
+
+
